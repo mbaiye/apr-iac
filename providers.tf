00b1f32399c0134167299a3c4f02e5cf01d7b6ac
Moyosore baiye <moyosorebaiye@gmail.com>
2024-06-23 20:15:27 +0100

    Added VPC resources

--- providers.tf
+++ providers.tf
@@ -1,15 +1,15 @@
 # configure aws provider to establish connection with aws account
 
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = "default"
 
   default_tags {
     tags = {
-        Project       = var.project_name
-        Environment = var.environment
-        Automation = "Terraform"
-}
-  
-}
+      Project     = var.project_name
+      Environment = var.environment
+      Automation  = "Terraform"
+    }
+
+  }
 }
--- variables.tf
+++ variables.tf
@@ -2,15 +2,51 @@
 
 variable "region" {
   description = "The AWS region to deploy resources"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "Project name"
-  type = string
+  type        = string
 }
 
 variable "environment" {
   description = "Environment"
-  type = string
+  type        = string
+}
+
+# VPC variables
+variable "vpc_cidr" {
+  description = "VPC cidr block"
+  type        = string
+}
+
+variable "public_subnet_cidr_az1" {
+  description = "Public subnet az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_cidr_az2" {
+  description = "Public subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_cidr_az1" {
+  description = "Private app subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_cidr_az2" {
+  description = "Private app subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_cidr_az1" {
+  description = "Private data subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_cidr_az2" {
+  description = "Private data subnet az2 cidr block"
+  type        = string
 }
